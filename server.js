430ba50c1151292c102cf0b3b04db679cbb0eb5e
Patrick Hennessey <pjhennessey89@gmail.com>
2024-08-30 10:55:48 -0500

    expanded file structure

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const path = require('path');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./config/connection');
 const app = express();
@@ -16,15 +17,15 @@ const sess = {
     })
 };
 
-app.use(session(sess));
-
 const hbs = exphbs.create({});
 
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./routes');)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
