e4689c5c5337863808fc12a69ed37cd095fc4b80
Patrick Hennessey <pjhennessey89@gmail.com>
2024-08-30 10:31:42 -0500

    Set up server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on PORT ${PORT}`));
+});
