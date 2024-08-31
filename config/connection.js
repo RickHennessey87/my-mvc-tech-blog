4c0acdd1413b43027fd3124b4313e4086d505e02
Patrick Hennessey <pjhennessey89@gmail.com>
2024-08-31 12:15:49 -0500

    Fixed bugs to ensure the server was running and html displaying

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'postgres',
-    port: 3001
+    port: 5432
 });
 
 module.exports = sequelize;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -3,13 +3,22 @@ const { BlogPost } = require('../models');
 
 router.get('/', async (req, res) => {
     try {
-        const blogPostData = await BlogPost.findAll({
-            attributes: ['id', 'title', 'content', 'date_created']
-        });
+        // const blogPostData = await BlogPost.findAll({
+        //     attributes: ['id', 'title', 'content', 'date_created']
+        // });
 
-        const blogPosts = blogPostData.map((post) => post.get({ plain: true }));
+        // const blogPosts = blogPostData.map((post) => post.get({ plain: true }));
 
-        res.render('home', {
+        const blogPosts = [
+            { title: 'First Post', content: 'This is the first post.', date_created: '2024-08-30' }
+        ];
+
+        // res.render('homepage', {
+        //     blogPosts,
+        //     loggedIn: req.session.loggedIn
+        // });
+
+        res.render('homepage', { 
             blogPosts,
             loggedIn: req.session.loggedIn
         });
