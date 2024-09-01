ed01f5a8f355a93afa38bb2f6176c255804a6c66
Patrick Hennessey <pjhennessey89@gmail.com>
2024-09-01 17:34:32 -0500

    Updated routes for signin

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -26,8 +26,4 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/signup', (req, res) => {
-    res.render('signup');
-});
-
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const homeRoutes = require('./homeRoutes');
 const authRoutes = require('./authRoutes');
+const userRoutes = require('./userRoutes');
 
 router.use('/', homeRoutes);
 router.use('/auth', authRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
